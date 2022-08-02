ad8d0a6e54419e80e7148a6a7c860ec622de08f7
Esmatullah Ahmadzai <ea.ahmadzai2020@gmail.com>
2022-08-02 15:47:11 +0430

    Fix linters

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import _ from "lodash";
-import "./style.css";
-import Icon from "./icon.png";
+import _ from 'lodash';
+import './style.css';
+import Icon from './icon.png';
 
 function component() {
-  const element = document.createElement("div");
+  const element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
   // Lodash, now imported by this script
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
-  element.classList.add("hello");
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
 
   // Add the image to our existing div.
   const myIcon = new Image();
